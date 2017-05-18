eeb9d3e7a11b0b5051e5eb85cd086a2a73f80b51
Jonathan Guthrie <jono@perfect.org>
2017-05-18 10:58:24 -0400

    remove minor versions

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ let package = Package(
     name: "SwiftCSV",
     targets: [],
     dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Curl.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2, minor: 0)
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Curl.git", majorVersion: 2),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2)
 		],
     exclude: []
 )
