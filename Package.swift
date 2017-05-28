b91ad58e8194499d6abfbd304c992845dd674f64
Jonathan Guthrie <jono@perfect.org>
2017-05-28 19:53:12 -0400

    case

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     targets: [],
     dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Curl.git", majorVersion: 2),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2)
 		],
     exclude: []
