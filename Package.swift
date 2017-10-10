0ca9b5f692f83edaa8d7c9bdc071758a67bf07b9
Jonathan Guthrie <jono@perfect.org>
2017-10-10 09:47:22 -0400

    update for swift 4

--- Package.swift
+++ Package.swift
@@ -1,13 +1,12 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-09-17 01:35:49 +0000
+// Date: 2017-10-10 13:46:27 +0000
 import PackageDescription
 let package = Package(
-    name: "SwiftCSV",
-    targets: [],
-    dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2)
-		],
-    exclude: []
+	name: "SwiftCSV",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 3),
+	]
 )
